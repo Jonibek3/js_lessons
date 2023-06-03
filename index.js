3d2221fe56827d8f59b50ee03e0e1085965e4958
Jonibek Sharifkulov <jonibeksharifkulov11@icloud.com>
2023-06-03 12:03:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// string
+// let str = "lorem ipsum dolor sit amet";
+
+// str.toLowerCase(),
+// str.toUpperCase(),
+// str.slice(5)
+// str.search("world") // index
+// str.slice(str.search("world"), str.search("world") + 5)
+// str.trim().charAt(2) // first letter
+// str.trim()[2]
+// str.split("a")
+// str.replace("dollllor", "sum")
+// str.length
+
+// number
+// let num = 20.49;
+// let num2 = 20.2;
+
+//   Math.ceil(num), // up
+//   Math.floor(num), // down
+//   Math.round(num) // logical
+//   20 + parseInt(num)
+//   20 + parseFloat(num)
+//   20 + +num
+//   Math.min(num, num2, 22, 12),
+//   Math.max(num, num2)
+//   Math.floor(Math.random() * 50)
